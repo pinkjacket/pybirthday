8aa97b86062b625c4db528ec8954aca87cd5a386
pinkjacket <33609459+pinkjacket@users.noreply.github.com>
2018-05-13 23:48:07 -0700

    basic layout done

---
+++ program.py
@@ -0,0 +1,22 @@
+def print_header():
+    pass
+
+
+def get_birthday():
+    pass
+
+
+def get_date_difference():
+    pass
+
+
+def print_birthday_info():
+    pass
+
+
+def main():
+    print_header()
+    bday = get_birthday()
+    now = None
+    days = get_date_difference(bday, now)
+    print_birthday_info(days)
